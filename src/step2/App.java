9e4e4ab82f71a926af9aae819cc18c29a038e696
tmdgsudl <leesh27955@gmail.com>
2026-01-12 12:51:17 +0900

    [Step2] 연산은 Calculator클래스/ 호출을 Main클래스 담당되도록 분리(+예외처리)

--- src/step2/App.java
+++ src/step2/App.java
@@ -8,16 +8,52 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         /* 반복문 시작 */
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = Integer.parseInt(sc.nextLine());
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.nextLine().charAt(0);
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = Integer.parseInt(sc.nextLine());
+        while (true) {
+            int num1;
+            int num2;
+            char operator;
+            while (true) {
+                try {
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    num1 = Integer.parseInt(sc.nextLine());
+                    break;
+                } catch (Exception e) {
+                    System.out.println("숫자를 입력하세요.");
+                }
+            }
+            while (true) {
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                operator = sc.nextLine().charAt(0);
 
-        int result = cal.calculate(num1, num2, operator);
-        System.out.println("결과: " + result);
+                if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
+                    break;
+                }
+                System.out.println("올바른 기호가 아닙니다.");
+            }
 
-        System.out.println("더 계산하시겠습니가? (exit 입력시 종료)");
+            while (true) {
+                try {
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    num2 = Integer.parseInt(sc.nextLine());
+                    if (operator == '/' && num2 == 0) {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                        continue;
+                    }
+                    break;
+                } catch (Exception e) {
+                    System.out.println("숫자를 입력하세요.");
+                }
+            }
+
+            int result = cal.calculate(num1, num2, operator);
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니가? (exit 입력시 종료)");
+            String answer = sc.nextLine();
+            if (answer.equals("exit")) {
+                break;
+            }
+            System.out.println("계산기를 종료합니다.");
+        }
     }
 }
