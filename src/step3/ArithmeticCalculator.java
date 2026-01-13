077aef5bde1cdd127a3091129e80ec835197f1cf
tmdgsudl <leesh27955@gmail.com>
2026-01-13 19:43:12 +0900

    [Ste3] enum 타입 생성(생성자, 게터)

--- src/step3/ArithmeticCalculator.java
+++ src/step3/ArithmeticCalculator.java
@@ -1,4 +1,57 @@
-package step3;
+package step2;
 
-public class ArithmeticCalculator {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    // 캡슐화 private 키워드 사용하여 접근제어(App 클래스의 main메서드로부터)
+    // 연산 결과 저장하는 컬레션 타입(ArratList) 생성
+    private List<Integer> history = new ArrayList<>();
+
+    int result = 0;
+
+    String operator;
+
+    public int calculate(int num1, int num2, char operator) {
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("올바른 연산자가 아닙니다.");
+        }
+        // 연산 결과값 저장
+//        history.add(result);   -> 저장하는 역할을 세터에게 넘겼음 *한 메서드당 하나의 기능을 맡는 것이 좋다.
+        return result;
+    }
+
+    // Getter 메서드
+    public List<Integer> getHistory() {
+        return this.history;
+    }
+
+    // Setter 메서드
+    public void setHistory(int newHistoryNumber) {
+        // 수정할 값을 따로 반환해서 어디 저장하지 않고 바로 history에 추가하는 것이 효율적.
+        this.history.add(newHistoryNumber);
+    }
+
+    // 삭제하는 메서드
+    // 가장 먼저 저장된 데이터 -> 배열의 원소..? 에서 0번인덱스 삭제하면 되나?
+    public void removeResult() {
+        history.remove(0);
+    }
 }
+
+
+
--- src/step3/OperatorType.java
+++ src/step3/OperatorType.java
@@ -1,6 +1,31 @@
 package step3;
+// TODO: enum 개념 조금 더 이해해보기.
+// 미리 정의된 상수들의 집합 + 기본적으로 public static final이라고 생각하면 된다. (final + 상수이기 때문에 모두 대문자로)
+// enum은 new 키워드로 객체 생성할 필요 없다. -> JVM이 프로그램 시작 시 미리 다 만들어두기 때문
+
+import java.util.Arrays;
 
 public enum OperatorType {
-    // step3 enum 시작 전 세팅
+    // enum 상수 (enum값, enum 인스턴스) 라고 한다.. 밑의 코드들이 있으면 실행시점에 JVM이 자동으로 객체 1개씩 생성.
+    PLUS('+'),
+    MINUS('-'),
+    MULTIPLY('*'),
+    DIVIDE('/');
+    // 이 객체들은 딱 한 번만 생성됨(싱글톤이라고 함)
 
+    // char(문자열) 형식의 symbol(연산기호) 필드 (속성) 추가
+
+    // 생성자
+    OperatorType(char operator) {
+        this.operator = operator;
+    }
+    private final char operator;
+
+    // 게터
+    public static OperatorType findOperator(char operator) {
+       return Arrays.stream(OperatorType.values())
+               .filter(o -> o.operator == operator)
+               .findFirst()
+               .orElseThrow(()-> new IllegalArgumentException("잘못된 연산입니다."));
+    }
 }
