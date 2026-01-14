f0feeef4f864a065641cf7d3b15eb569a16fa36c
tmdgsudl <leesh27955@gmail.com>
2026-01-14 17:13:10 +0900

    [Step3]변수명 수정 및 코드리뷰

--- src/step3/Menu.java
+++ src/step3/Menu.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 
 public class Menu {
 
+    // Menu 클래스에서만 사용할 것.
     private final Scanner sc = new Scanner(System.in);
+    // ArithmeticCalculator 클래스의 객체(calculator)를 생성함으로서 그 메서드를 호출할 수 있다. (calculator.메서드명())
     private final ArithmeticCalculator calculator = new ArithmeticCalculator();
 
+    // 메뉴 실행 메서드
     public void start() {
         System.out.println("=== 계산기 메뉴 ===");
         System.out.println("1. 계산하기");
@@ -18,9 +21,10 @@ public class Menu {
 
         // TODO: 나중에 do - while로 바꿔볼까..?
         while (true) {
-            System.out.print("계산기 메뉴를 선택하시오: ");
             int answer = 0;
+
             try {
+                System.out.print("계산기 메뉴를 선택하시오: ");
                 answer = Integer.parseInt(sc.nextLine());
             } catch (NumberFormatException e) {
                 System.out.println("숫자를 입력하세요.");
@@ -32,10 +36,10 @@ public class Menu {
                     calculate();
                     break;
                 case 2:
-                    getHistory();
+                    bringHistory();
                     break;
                 case 3:
-                    removeResult();
+                    deleteFirst();
                     break;
                 case 0: {
                     System.out.println("종료하시겠습니가? (exit 입력 시 종료): ");
@@ -51,82 +55,89 @@ public class Menu {
         }
     }
 
+    // 계산 메서드
     public void calculate() {
-        double num1 = 0;
-        double num2 = 0;
-        double result = 0;
-        char operator = 0;
-
         while (true) {
-            try {
-                System.out.print("첫 번째 숫자를 입력하세요: ");
-                num1 = Double.parseDouble(sc.nextLine());
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("숫자를 입력하세요.");
-            }
-        }
-
-        while (true) {
-            System.out.print("사칙연산 기호를 입력하세요 (+, -, *, /): ");
-            operator = sc.nextLine().charAt(0);
-            if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
-                break;
-            } else {
-                System.out.println("올바른 사칙연산 기호가 아닙니다.");
-            }
-        }
-
-        while (true) {
-            try {
-                System.out.print("두 번째 숫자를 입력하세요: ");
-                num2 = Double.parseDouble(sc.nextLine());
-                if (operator == '/' && num2 == 0) {
-                    System.out.println("0으로 나눌 수 없습니다.");
-                    continue;
+            double num1 = 0;
+            double num2 = 0;
+            double result = 0;
+            char operator = 0;
+            while (true) {
+                try {
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    num1 = Double.parseDouble(sc.nextLine());  // 입력값(문자열)을 실수값으로 변환
+                    break;                                    // 스캐너로 입력받은 값은 항상 문자열임
+                } catch (NumberFormatException e) {          // 입력값이 변환이 안 될 경우 예외발생 (ex. "abc", "12a", ...)
+                    System.out.println("숫자를 입력하세요.");
                 }
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("숫자를 입력하세요.");
             }
-        }
-        result = calculator.calculate(num1, num2, operator);
-        calculator.setHistory(result);
-        System.out.println("결과: " + result);
 
-        while (true) {
-            System.out.println("더 계산하시겠습니까? (y/n): ");
-            String reStart = sc.nextLine();
-            if (reStart.equals("y")) {
-                break;
-            } else if (reStart.equals("n")) {
-                return;
-            } else {
-                System.out.println("y 또는 n만 입력해주세요.");
+            while (true) {
+                System.out.print("사칙연산 기호를 입력하세요 (+, -, *, /): ");
+                operator = sc.nextLine().charAt(0); // 입력받은 문자열 중 첫 번째 문자(인덱스 0)만 추출하겠다.
+                if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
+                    break;
+                } else {
+                    System.out.println("올바른 사칙연산 기호가 아닙니다.");
+                }
+            }
+
+            while (true) {
+                try {
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    num2 = Double.parseDouble(sc.nextLine());
+                    if (operator == '/' && num2 == 0) {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                        continue;
+                    }
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("숫자를 입력하세요.");
+                }
+            }
+            // 객체화를 했기 때문에 메서드 호출 가능
+            result = calculator.calculate(num1, num2, operator);
+            calculator.setHistory(result);
+            System.out.println("결과: " + result);
+
+            while (true) {
+                System.out.println("더 계산하시겠습니까? (y/n): ");
+                String reStart = sc.nextLine();
+                if (reStart.equalsIgnoreCase("y")) {
+                    break;
+                } else if (reStart.equalsIgnoreCase("n")) {
+                    return;
+                } else {
+                    System.out.println("y 또는 n만 입력해주세요.");
+                }
             }
         }
+
     }
 
-    public void getHistory() {
+    // 2번 입력시 기록 조회. (기록이 없을 때 예외처리)
+    // ArithmeticCalculator 클래스의 메서드를 호출해 결과기록들을 가져온다.
+    public void bringHistory() {
         if (calculator.getHistory().isEmpty()) {
             System.out.println("계산 기록이 없습니다.");
             return;
-        } else {
-            System.out.println("--계산 기록--");
-            System.out.println(calculator.getHistory());
         }
+        System.out.println("--계산 기록--");
+        System.out.println(calculator.getHistory());
+
     }
 
-    public void removeResult() {
+    // 3번 입력시 가장 첫 기록 삭제. (기록이 없을 때 예외처리)
+    public void deleteFirst() {
         // 계산 기록이 없는데 삭제 요청할 시 예외처리
         if (calculator.getHistory().isEmpty()) {
             System.out.println("계산 기록이 없습니다.");
             return;
-        } else {
-            calculator.removeResult();
-            System.out.println("삭제되었습니다.");
-            System.out.println("--계산 기록--");
-            System.out.println(calculator.getHistory());
         }
+        // 가장 처음 기록을 삭제하는 ArithmeticCalculator 클래스의 메서드 호출
+        calculator.removeResult();
+        System.out.println("삭제되었습니다.");
+        System.out.println("--계산 기록--");
+        System.out.println(calculator.getHistory());
     }
 }
